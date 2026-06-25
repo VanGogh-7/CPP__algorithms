90bbdf9869d940eedea55328daeb0f2c0a736d13
van-gogh <weiqifan0524@gmail.com>
2026-06-25 21:23:44 +0800

    day4.0; Jun-24.

--- main.cpp
+++ main.cpp
@@ -1,35 +1,18 @@
 #include <iostream>
-#include <istream>
-#include <memory>
-
-struct Point_2d {
-    int x;
-    int y;
-    void print() {
-        std::cout << x << "," << y << std::endl;
-    }
-};
-
-class Point_3d {
-    int x;
-    int y;
-    int z;
-    void print() {
-        std::cout << x << "," << y << ',' << std::endl;
-    }
-};
-
+#include <mutex>
+#include <thread>
+#include <vector>
 
 int main() {
-    std::unique_ptr<int[]> ptr1(new int[10]);
-    auto ptr2 = std::make_unique<int[]>(10);
+    std::mutex mtx;
+    auto print_thread_id = [&mtx](int id) {
+        std::lock_guard<std::mutex> lock(mtx);
+        std::cout << id << '\n';
+    };
 
-    std::shared_ptr<int[]> ptr3(new int[10]);
-    auto ptr4 = ptr3;
-
-    for (int i = 0; i < 10; i++) {
-        ptr1[i] = i * i;
+    std::vector<std::jthread> threads;
+    for (int i = 0; i < 5; ++i) {
+        threads.emplace_back(print_thread_id, i);
     }
-    std::cout << ptr1[4] << std::endl;
     return 0;
 }
