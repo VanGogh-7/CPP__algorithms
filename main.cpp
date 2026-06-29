21d77a69f6a5876ad1a6a89856619bf0e0102eae
van-gogh <weiqifan0524@gmail.com>
2026-06-29 19:31:31 +0800

    day5.0; Jun-29.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,35 @@
 #include <iostream>
-#include <mutex>
-#include <thread>
+#include <ostream>
 #include <vector>
 
-int main() {
-    std::mutex mtx;
-    auto print_thread_id = [&mtx](int id) {
-        std::lock_guard<std::mutex> lock(mtx);
-        std::cout << id << '\n';
-    };
+class solution {
+public:
+    std::vector<int> find_error_nums(std::vector<int> &nums) {
+        const int n = static_cast<int>(nums.size());
+        std::vector<int> count(n + 1, 0);
 
-    std::vector<std::jthread> threads;
-    for (int i = 0; i < 5; ++i) {
-        threads.emplace_back(print_thread_id, i);
+        for (const int num: nums) {
+            ++count[num];
+        }
+        int duplicated = -1;
+        int missing = -1;
+
+        for (int num = 1; num <= n; ++num) {
+            if (count[num] == 2) {
+                duplicated = num;
+            } else if (count[num] == 0) {
+                missing = num;
+            }
+        }
+        return {duplicated, missing};
+    }
+};
+
+int main() {
+    std::vector<int> nums = {1, 2, 3, 3, 4};
+    std::vector<int> n = solution().find_error_nums(nums);
+    for (int num: n) {
+        std::cout << num << std::endl;
     }
     return 0;
 }
